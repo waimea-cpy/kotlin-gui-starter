d5e804b1df6b71e253e33720e77f5412ccc6138b
Steve Copley <steve.copley@waimea.school.nz>
2025-03-14 15:10:00 +1300

    Created demo

--- src/Main.kt
+++ src/Main.kt
@@ -34,16 +34,36 @@ fun main() {
  * stored, plus any application logic functions
  */
 class App() {
-    // Constants defining any key values
-    val MAX_CLICKS = 10
-
     // Data fields
-    var clicks = 0
+    val cats = mutableListOf<String>()
+    var currentCat = 0
+
+    // Setup the app model
+    init {
+        cats.add("Gary")
+        cats.add("Sally")
+        cats.add("Harry")
+        cats.add("Tina")
+        cats.add("Phil")
+    }
 
     // Application logic functions
-    fun updateClickCount() {
-        clicks++
-        if (clicks > MAX_CLICKS) clicks = MAX_CLICKS
+
+    fun nextCat() {
+        currentCat++
+        if (currentCat > cats.size - 1)
+            currentCat = cats.size - 1
+    }
+
+    fun prevCat() {
+        currentCat--
+        if (currentCat < 0)
+            currentCat = 0
+    }
+
+    fun newCat(newCatName: String) {
+        cats.add(newCatName)
+        currentCat = cats.lastIndex
     }
 }
 
@@ -56,8 +76,12 @@ class App() {
 class MainWindow(val app: App) : JFrame(), ActionListener {
 
     // Fields to hold the UI elements
-    private lateinit var greetingLabel: JLabel
-    private lateinit var helloButton: JButton
+    private lateinit var catNumLabel: JLabel
+    private lateinit var catNameLabel: JLabel
+    private lateinit var nextButton: JButton
+    private lateinit var prevButton: JButton
+    private lateinit var nameText: JTextField
+    private lateinit var addButton: JButton
 
     /**
      * Configure the UI and display it
@@ -68,14 +92,16 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
 
         setLocationRelativeTo(null)     // Centre the window
         isVisible = true                // Make it visible
+
+        updateView()
     }
 
     /**
      * Configure the main window
      */
     private fun configureWindow() {
-        title = "Kotlin Swing GUI Demo"
-        contentPane.preferredSize = Dimension(600, 350)
+        title = "Cat Scroller 5000"
+        contentPane.preferredSize = Dimension(600, 500)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -87,19 +113,43 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      * Populate the UI with UI controls
      */
     private fun addControls() {
-        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
+        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 64)
 
-        greetingLabel = JLabel("Hello, World!")
-        greetingLabel.horizontalAlignment = SwingConstants.CENTER
-        greetingLabel.bounds = Rectangle(50, 50, 500, 100)
-        greetingLabel.font = baseFont
-        add(greetingLabel)
+        catNumLabel = JLabel("#00")
+        catNumLabel.horizontalAlignment = SwingConstants.RIGHT
+        catNumLabel.bounds = Rectangle(50, 50, 100, 100)
+        catNumLabel.font = baseFont
+        add(catNumLabel)
 
-        helloButton = JButton("Click Me!")
-        helloButton.bounds = Rectangle(50,200,500,100)
-        helloButton.font = baseFont
-        helloButton.addActionListener(this)     // Handle any clicks
-        add(helloButton)
+        catNameLabel = JLabel("CAT NAME")
+        catNameLabel.horizontalAlignment = SwingConstants.LEFT
+        catNameLabel.bounds = Rectangle(200, 50, 350, 100)
+        catNameLabel.font = baseFont
+        add(catNameLabel)
+
+        nextButton = JButton("⯈")
+        nextButton.bounds = Rectangle(325,200,225,100)
+        nextButton.font = baseFont
+        nextButton.addActionListener(this)     // Handle any clicks
+        add(nextButton)
+
+        prevButton = JButton("⯇")
+        prevButton.bounds = Rectangle(50,200,225,100)
+        prevButton.font = baseFont
+        prevButton.addActionListener(this)     // Handle any clicks
+        add(prevButton)
+
+        nameText = JTextField()
+        nameText.bounds = Rectangle(50,350,300,100)
+        nameText.font = baseFont
+        nameText.addActionListener(this)
+        add(nameText)
+
+        addButton = JButton("+")
+        addButton.bounds = Rectangle(400,350,150,100)
+        addButton.font = baseFont
+        addButton.addActionListener(this)     // Handle any clicks
+        add(addButton)
     }
 
 
@@ -108,13 +158,16 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      * of the application model
      */
     fun updateView() {
-        if (app.clicks == app.MAX_CLICKS) {
-            greetingLabel.text = "Max clicks reached!"
-            helloButton.isEnabled = false
-        }
-        else {
-            greetingLabel.text = "You clicked ${app.clicks} times"
-        }
+        val catNum = app.currentCat + 1
+        val catName = app.cats[app.currentCat]
+
+        catNumLabel.text = "#$catNum"
+        catNameLabel.text = catName
+
+        prevButton.isEnabled = (app.currentCat > 0)
+        nextButton.isEnabled = (app.currentCat < app.cats.size - 1)
+
+        nameText.text = ""
     }
 
     /**
@@ -124,9 +177,23 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      */
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.source) {
-            helloButton -> {
-                app.updateClickCount()
+            nextButton -> {
+                // Update Model
+                app.nextCat()
+                // And the view
                 updateView()
+            }
+            prevButton -> {
+                app.prevCat()
+                updateView()
+            }
+
+            addButton, nameText -> {
+                if (nameText.text.isNotBlank()) {
+                    val name = nameText.text
+                    app.newCat(name)
+                    updateView()
+                }
             }
         }
     }
