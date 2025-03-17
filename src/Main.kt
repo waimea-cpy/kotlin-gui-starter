4628a76b6569de61448c76d89b1570f33cb1a1f0
Steve Copley <steve.copley@waimea.school.nz>
2025-03-18 09:57:46 +1300

    Demo created

--- src/Main.kt
+++ src/Main.kt
@@ -29,22 +29,34 @@ fun main() {
 
 
 /**
+ * Cat class
+ */
+class Cat(val name: String, val colour: String) {
+    var alive = true
+
+    fun kill() {
+        alive = false
+    }
+}
+
+
+/**
  * The application class (model)
  * This is the place where any application data should be
  * stored, plus any application logic functions
  */
 class App() {
     // Data fields
-    val cats = mutableListOf<String>()
+    val cats = mutableListOf<Cat>()
     var currentCat = 0
 
     // Setup the app model
     init {
-        cats.add("Gary")
-        cats.add("Sally")
-        cats.add("Harry")
-        cats.add("Tina")
-        cats.add("Phil")
+        cats.add(Cat("Gary", "Ginger"))
+        cats.add(Cat("Sally", "Black"))
+        cats.add(Cat("Harry", "White"))
+        cats.add(Cat("Tina", "Tabby"))
+        cats.add(Cat("Phil", "Tabby"))
     }
 
     // Application logic functions
@@ -61,9 +73,14 @@ class App() {
             currentCat = 0
     }
 
-    fun newCat(newCatName: String) {
-        cats.add(newCatName)
+    fun newCat(newCatName: String, newCatColour: String) {
+        cats.add(Cat(newCatName,newCatColour))
         currentCat = cats.lastIndex
+    }
+
+    fun killCat() {
+        val cat = cats[currentCat]
+        cat.kill()
     }
 }
 
@@ -78,9 +95,13 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
     // Fields to hold the UI elements
     private lateinit var catNumLabel: JLabel
     private lateinit var catNameLabel: JLabel
+    private lateinit var catColourLabel: JLabel
+    private lateinit var catAliveLabel: JLabel
     private lateinit var nextButton: JButton
     private lateinit var prevButton: JButton
+    private lateinit var killButton: JButton
     private lateinit var nameText: JTextField
+    private lateinit var colourText: JTextField
     private lateinit var addButton: JButton
 
     /**
@@ -101,7 +122,7 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      */
     private fun configureWindow() {
         title = "Cat Scroller 5000"
-        contentPane.preferredSize = Dimension(600, 500)
+        contentPane.preferredSize = Dimension(900, 500)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -113,25 +134,31 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      * Populate the UI with UI controls
      */
     private fun addControls() {
-        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 64)
+        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 48)
 
-        catNumLabel = JLabel("#00")
-        catNumLabel.horizontalAlignment = SwingConstants.RIGHT
-        catNumLabel.bounds = Rectangle(50, 50, 100, 100)
+        catNumLabel = JLabel("00")
+        catNumLabel.horizontalAlignment = SwingConstants.CENTER
+        catNumLabel.bounds = Rectangle(50, 50, 50, 100)
         catNumLabel.font = baseFont
         add(catNumLabel)
 
-        catNameLabel = JLabel("CAT NAME")
+        catNameLabel = JLabel("NAME")
         catNameLabel.horizontalAlignment = SwingConstants.LEFT
-        catNameLabel.bounds = Rectangle(200, 50, 350, 100)
+        catNameLabel.bounds = Rectangle(150, 50, 250, 100)
         catNameLabel.font = baseFont
         add(catNameLabel)
 
-        nextButton = JButton("⯈")
-        nextButton.bounds = Rectangle(325,200,225,100)
-        nextButton.font = baseFont
-        nextButton.addActionListener(this)     // Handle any clicks
-        add(nextButton)
+        catColourLabel = JLabel("COLOUR")
+        catColourLabel.horizontalAlignment = SwingConstants.LEFT
+        catColourLabel.bounds = Rectangle(450, 50, 250, 100)
+        catColourLabel.font = baseFont
+        add(catColourLabel)
+
+        catAliveLabel = JLabel("Y")
+        catAliveLabel.horizontalAlignment = SwingConstants.CENTER
+        catAliveLabel.bounds = Rectangle(750, 50, 100, 100)
+        catAliveLabel.font = baseFont
+        add(catAliveLabel)
 
         prevButton = JButton("⯇")
         prevButton.bounds = Rectangle(50,200,225,100)
@@ -139,14 +166,32 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         prevButton.addActionListener(this)     // Handle any clicks
         add(prevButton)
 
+        nextButton = JButton("⯈")
+        nextButton.bounds = Rectangle(325,200,225,100)
+        nextButton.font = baseFont
+        nextButton.addActionListener(this)     // Handle any clicks
+        add(nextButton)
+
+        killButton = JButton("Dead")
+        killButton.bounds = Rectangle(625,200,225,100)
+        killButton.font = baseFont
+        killButton.addActionListener(this)     // Handle any clicks
+        add(killButton)
+
         nameText = JTextField()
         nameText.bounds = Rectangle(50,350,300,100)
         nameText.font = baseFont
         nameText.addActionListener(this)
         add(nameText)
 
+        colourText = JTextField()
+        colourText.bounds = Rectangle(400,350,300,100)
+        colourText.font = baseFont
+        colourText.addActionListener(this)
+        add(colourText)
+
         addButton = JButton("+")
-        addButton.bounds = Rectangle(400,350,150,100)
+        addButton.bounds = Rectangle(750,350,100,100)
         addButton.font = baseFont
         addButton.addActionListener(this)     // Handle any clicks
         add(addButton)
@@ -159,15 +204,25 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      */
     fun updateView() {
         val catNum = app.currentCat + 1
-        val catName = app.cats[app.currentCat]
+        val cat = app.cats[app.currentCat]
 
-        catNumLabel.text = "#$catNum"
-        catNameLabel.text = catName
+        val aliveColour = Color.WHITE
+        val deadColour = Color(255, 170, 170)
+
+        catNumLabel.text = catNum.toString()
+        catNameLabel.text = cat.name
+        catColourLabel.text = cat.colour
+        catAliveLabel.text = if (cat.alive) "☻" else "☠"
+
+        catNumLabel.foreground = if (cat.alive) aliveColour else deadColour
+        catNameLabel.foreground = if (cat.alive) aliveColour else deadColour
+        catColourLabel.foreground = if (cat.alive) aliveColour else deadColour
+        catAliveLabel.foreground = if (cat.alive) aliveColour else deadColour
 
         prevButton.isEnabled = (app.currentCat > 0)
         nextButton.isEnabled = (app.currentCat < app.cats.size - 1)
 
-        nameText.text = ""
+        killButton.isEnabled = cat.alive
     }
 
     /**
@@ -188,11 +243,23 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
                 updateView()
             }
 
-            addButton, nameText -> {
-                if (nameText.text.isNotBlank()) {
+            killButton -> {
+                app.killCat()
+                updateView()
+            }
+
+            addButton,
+            nameText,
+            colourText -> {
+                if (nameText.text.isNotBlank() && colourText.text.isNotBlank()) {
                     val name = nameText.text
-                    app.newCat(name)
+                    val colour = colourText.text
+                    app.newCat(name, colour)
+
+                    nameText.text = ""
+                    colourText.text = ""
                     updateView()
+                    nameText.requestFocus()
                 }
             }
         }
